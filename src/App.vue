62e396a691f3478fcabedff333f7f1ea6e02c908
HenriqueChepli <henriquechepli@gmail.com>
2024-07-27 13:44:09 -0300

    FEAT: App.vue Feito

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, reactive, ref } from 'vue';
+import FormView from '@/components/FormView.vue';
+import DadosView from '@/components/DadosView.vue';
+
+const dados = ref('')
+
+function save(item) {
+  console.log(item)
+  dados.value = item
+}
+
+const mostrarDados = computed(() => {
+  return dados.value !== ''
+})
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <FormView @salvar="save" />
+  </div>
+  <div>
+    <DadosView v-if="mostrarDados" :Dados="dados" />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+div{
+  margin: 0 50px;
 }
 </style>
